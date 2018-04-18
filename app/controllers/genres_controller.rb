58508db4706c66d44dd47a0ab6fac8e950596d27
wenhao <wenhao@freeloops.local>
2018-04-18 09:01:40 +0800

    fixed the song part and removed temp files

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -2,7 +2,7 @@ class GenresController < ApplicationController
 	# /songs/:song_id/genres
 	def index
 		@genres = Genre.joins(:genres_songs).where('genres_songs.song_id in (?)',params[:song_id])
-		@song = Song.connection.select_all('SELECT name FROM songs INNER JOIN genres_songs on songs.id = genres_songs.song_id WHERE songs.id=' +params[:song_id])
+		@song = Song.find(params[:song_id]);
 	end
 
 	private
